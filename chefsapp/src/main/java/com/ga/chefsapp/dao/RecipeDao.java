70690681ead16e2ac54771771f1cef00e2c0dfb8
fatimah2020 <j.faqihi@hotmail.com>
2021-01-31 14:30:18 +0300

    adding ordaring recipe by rating

--- chefsapp/src/main/java/com/ga/chefsapp/dao/RecipeDao.java
+++ chefsapp/src/main/java/com/ga/chefsapp/dao/RecipeDao.java
@@ -1,6 +1,21 @@
 package com.ga.chefsapp.dao;
+
 import com.ga.chefsapp.model.Recipe;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
 public interface RecipeDao extends CrudRepository<Recipe, Integer> {
-	public Recipe findById(int id);
+	@Query(value = "SELECT * FROM recipe order by\n"
+	  		+ "									(\n"
+	  		+ "                                    select avg(rating)\n"
+	  		+ "                                    from rate \n"
+	  		+ "                                    where recipe.id =  fk_recipe_id\n"
+	  		+ "                                    group by fk_recipe_id\n"
+	  		+ "									\n"
+	  		+ "                                ) DESC",
+	            nativeQuery=true)
+	public Iterable<Recipe> findByOrderedRating();
+	    public Recipe findById(int id);
+	    
 }
